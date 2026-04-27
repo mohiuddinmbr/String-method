2182334048747a4576e31688263cb6f3f29e5217
mohiuddinmbr <mohiuddinmbr6@gmail.com>
2026-04-27 13:35:19 -0700

    assigment is panding

---
+++ app.js
@@ -0,0 +1,76 @@
+// ----- 1 -----
+
+// function name(name){
+//     console.log(`your name is ${name}`);
+// }
+// name("BALACH".toLocaleLowerCase())
+
+// ---- 2 ----
+
+// let greet = "hello"
+
+// console.log(greet.toLocaleUpperCase());
+
+// ---- 3 -----
+
+// let xyz = "hello world"
+
+// console.log(xyz.length);
+
+// ------ 4 ------
+// function id(name){
+//     console.log(`first number of your name is ${name}`);
+    
+// }
+// id("faraz".charAt(0))
+
+// ---- 5 ---- 
+
+// let js = "java script"
+
+// console.log(js.length);
+// console.log(js.charAt(11));
+
+// ---- 6 -----
+
+// let sentence = `an apple is very good for our health.`
+
+// console.log(sentence.indexOf("apple"));
+
+// ---- 7 -----
+
+
+// let sentence = `an apple is very good for our health.`
+
+// console.log(sentence.indexOf("very"));
+// console.log(sentence.slice(12, 16));
+
+// ------ 8 -----
+
+// let sentence = `an apple is very good for our health.`
+
+// console.log(sentence.replace("good" , "bad"));
+
+// ----- 9 -----
+
+// let sentence = `an apple is very good for our health.`
+
+// console.log(sentence.replaceAll("e", "*"));
+
+// ----- 10 -----
+
+// let sentence = `an apple is very good for our health.`
+
+// console.log(sentence.split(" "));
+
+// ------ 11 -----
+
+// let name = "mohiuddin"
+
+// function chap(index){
+//     return index.charAt(0).toLocaleUpperCase()  + index.slice(1)
+// }
+// console.log(chap(name));
+
+// ---- 12 ----
+
